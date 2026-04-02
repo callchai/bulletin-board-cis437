5a8aa7a3f0cf954b4f3dff995aa753b053153376
Chai Callow <chaibc33@gmail.com>
2026-04-02 15:04:13 -0400

    Fix term load

--- static/js/term-modal.js
+++ static/js/term-modal.js
@@ -26,27 +26,27 @@ const COLORS = [
     {bg:'#f9c5d1',author:'#a0253a'},
 ];
 
-const QUOTES = [
-    `Honor your alias, for it is your identity.`,
-    `"I am fond of boards. Posters look up to us. Trolls look down on us. Boards treat us as equals,"   - Clavicular`,
-    `<strong>Board 3:16</strong> - For the Board so loved the world that it gave its one and only Board, that whoever post in it shall not be forgotten but echo eternally.`,
-    `<strong>Board 37:13</strong> - but the Board laughs at the trolls, for it knows their day is nigh.`,
-    `<strong>Bulletinippians 4:13</strong> - I can post everything through it who gives me strength.`,
-    `<strong>1 Boarder 5:7</strong> - Be joyful in hope, patient in affliction, faithful in posting.`,
-    `<strong>Boardverbs 3:5-3:6</strong> - Trust in the BOARD with all your heart and lean not on your own understanding; in all your ways submit to it, and it will make your posts straight.`,
-    `The identity you have been given is <strong>brimming with countless possibilities</strong>. 
-    Your input breathes life into the Board, and will bring you glory. 
-    If you post with honor and creativity, the Board will answer back in kind.`,
-    `Are you ready to post? The Board awaits your contribution. However,
-    there is nothing to fear. Have faith. As long as you <strong>post accordingly and without malice,</strong> it
-    will bring you and your fellow boarders favorable fortune.`,
-    `Dear Bulletin Board, I posted to you, but you still ain't callin'.
-    I left my post, my name, and my number at the bottom. I posted two notes back in autumn, you must not have got 'em. 
-    There probably was a problem with the server or something. 
-    Sometimes I scribble IP addresses too sloppy when I jot 'em.`,
-    /*`<strong>Let it be known:</strong> no poster shall utter the blasphemies of Lola Young, Benson Boone, or Olivia Rodrigo, for their 
-    cries are an abomination to the Board, and those who speak of them shall be cast aside.`,*/
-];
+    const QUOTES = [
+        `Honor your alias, for it is your identity.`,
+        `"I am fond of boards. Posters look up to us. Trolls look down on us. Boards treat us as equals,"   - Clavicular`,
+        `<strong>Board 3:16</strong> - For the Board so loved the world that it gave its one and only Board, that whoever post in it shall not be forgotten but echo eternally.`,
+        `<strong>Board 37:13</strong> - but the Board laughs at the trolls, for it knows their day is nigh.`,
+        `<strong>Bulletinippians 4:13</strong> - I can post everything through it who gives me strength.`,
+        `<strong>1 Boarder 5:7</strong> - Be joyful in hope, patient in affliction, faithful in posting.`,
+        `<strong>Boardverbs 3:5-3:6</strong> - Trust in the BOARD with all your heart and lean not on your own understanding; in all your ways submit to it, and it will make your posts straight.`,
+        `The identity you have been given is <strong>brimming with countless possibilities</strong>. 
+        Your input breathes life into the Board, and will bring you glory. 
+        If you post with honor and creativity, the Board will answer back in kind.`,
+        `Are you ready to post? The Board awaits your contribution. However,
+        there is nothing to fear. Have faith. As long as you <strong>post accordingly and without malice,</strong> it
+        will bring you and your fellow boarders favorable fortune.`,
+        `Dear Bulletin Board, I posted to you, but you still ain't callin'.
+        I left my post, my name, and my number at the bottom. I posted two notes back in autumn, you must not have got 'em. 
+        There probably was a problem with the server or something. 
+        Sometimes I scribble IP addresses too sloppy when I jot 'em.`,
+        /*`<strong>Let it be known:</strong> no poster shall utter the blasphemies of Lola Young, Benson Boone, or Olivia Rodrigo, for their 
+        cries are an abomination to the Board, and those who speak of them shall be cast aside.`,*/
+    ];
 
 /*
 This will generate the name and Color.
@@ -105,8 +105,12 @@ function showWelcomeModal() {
 }
 
 enterBtn.addEventListener('click', () => {
+    const welcomeModal = document.getElementById('welcome-modal');
     welcomeModal.classList.remove('show');
-    setTimeout(() => {welcomeModal.style.display = 'none';}, 300);});
+    setTimeout(() => {
+        welcomeModal.style.display = 'none';
+    }, 300);
+});
 
 function showWelcomeBackModal() {
     const modal = document.getElementById('welcomeback-modal');
@@ -117,7 +121,9 @@ function showWelcomeBackModal() {
     document.getElementById('welcomeback-enter').addEventListener('click', () => {
         modal.classList.remove('show');
         setTimeout(() => {
-            modal.style.display = 'none';}, 300);});
+            modal.style.display = 'none';
+        }, 300);
+    });
 }
 
 initBoard(name, userColor);
